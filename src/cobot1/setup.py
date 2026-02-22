673a644706e655ed174c81df1d7c40dc416fe0cf
Jeonguks <ju460648@gmail.com>
2026-02-22 23:37:03 +0900

    manual merge : TT-23->TT-1

---
+++ src/cobot1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'cobot1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sa',
+    maintainer_email='jsa01jsa15@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'fire=cobot1.firebase_bridge:main',
+            'no=cobot1.testno:main',
+        ],
+    },
+)
